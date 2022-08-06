addac911341bbdf99977a8057b8f80d1ad9edf9b
Eric Qian <lavave@gmail.com>
2022-08-06 13:38:41 +0200

    add show_command_duration

--- config.fish
+++ config.fish
@@ -1,3 +1,15 @@
+# show command duration when a command takes more than X milliseconds,
+# or when the value of SHOW_COMMAND_DURATION is not empty:
+function show_command_duration --on-event fish_postexec
+  set -q COMMAND_DURATION_THRESHOLD_MS; or set COMMAND_DURATION_THRESHOLD_MS 5000
+  if test $CMD_DURATION -gt $COMMAND_DURATION_THRESHOLD_MS
+    or test -n "$SHOW_COMMAND_DURATION"
+      set duration (echo "$CMD_DURATION 1000" | awk '{printf "%.3fs", $1 / $2}')
+      echo "Duration: $duration"
+      set CMD_DURATION 0
+  end
+end
+
 if status is-interactive
   # Commands to run in interactive sessions can go here
   # Path for Homebrew (Apple Silicon)
