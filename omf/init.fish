e4cfd38c7d3881ccaa8264c7ca9b36902b58c336
Eric Qian <lavave@gmail.com>
2022-08-10 17:40:31 +0200

    add missing workaround for python fork

--- omf/init.fish
+++ omf/init.fish
@@ -2,6 +2,7 @@ set -xg LC_ALL en_US.UTF-8
 set -xg LANG en_US.UTF-8
 
 set -xg PYTHONDONTWRITEBYTECODE 1
+set -xg OBJC_DISABLE_INITIALIZE_FORK_SAFETY YES
 
 set LOCAL_FISH $OMF_CONFIG/local.fish
 if test -f $LOCAL_FISH
