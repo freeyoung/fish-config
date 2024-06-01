53d0e5b9d5492f7c8ee451ef023765049d1821e6
Eric Qian <lavave@gmail.com>
2024-06-01 09:52:00 +0200

    add kp, ks, virtualfish

---
+++ functions/kp.fish
@@ -0,0 +1,3 @@
+function kp --wraps='kubectl config use-context production-eric' --description 'alias kp kubectl config use-context production-eric'
+  kubectl config use-context production-eric;
+end
---
+++ functions/ks.fish
@@ -0,0 +1,3 @@
+function ks --wraps='kubectl config use-context staging-eric' --description 'alias ks kubectl config use-context staging-eric'
+  kubectl config use-context staging-eric;
+end
